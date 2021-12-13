f54b033adeb0ce8710f5d338d79a2ebe95049bb9
Yad <yad-1999@hotmail.com>
2021-12-13 18:37:35 +0300

    Setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,9 @@ module.exports = (env, argv) => {
         entry: './src/app.js',
         mode: 'development',
         output: {
-            path: path.join(__dirname, 'public'),
-            filename: 'bundle.js'
+            path: path.join(__dirname, 'public', 'dist'),
+            filename: 'bundle.js',
+            publicPath: "/dist/"
         },
         module: {
             rules: [{
